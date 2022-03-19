a604d9b79dc3711f7af0ecef921f4d84eb5871dc
woleekanola <woleekanola@gmail>
2022-03-19 16:54:02 +0100

    shapes dropping with awkward animation

--- app.js
+++ app.js
@@ -1,17 +1,112 @@
 const canvas = document.getElementById('tetris')
-const pixel = 50
+const pixel = 40
 const ctx = canvas.getContext('2d')
-canvas.width = pixel * 12
-canvas.height = pixel * 16
+canvas.width = 600
+canvas.height = 800
+
+
+const rows = canvas.height / pixel
+const cols = canvas.width / pixel
+currPlayer = {
+    shape: [],
+    posX: ((cols / 2) - 1) * pixel,
+    posY: 0,
+    collided: false,
+    canStart: true,
+    inPlay: true,
+    time: 0
+}
+
+let gameMatrix = []
+
+for (i = 0; i < rows; i++) {
+    let cll = []
+    for (j = 0; j < cols; j++) {
+        cll.push(0)
+    }
+    gameMatrix.push(cll)
+}
+
+// console.table(gameMatrix)
+
 let level = 1
 let speed = 10
 let frame = 0
 let score = 0
+const calc = (start, distX, distY) => {
+    let dist = 0
+    if (distX) {
+        dist = distX
+    }
+    if (distX == 'y') {
 
-const shapeModels = [
-    [0, 0, 0, 0, 1, 0, 1, 1, 1]
+        dist = distY + 1
+            // console.log('fgf ' + dist)
+    }
+    return ((start + dist) * pixel)
+}
+
+const player = [
+    [
+        [0, 0, 0],
+        [0, 1, 0],
+        [1, 1, 1]
+    ],
+    [
+        [2, 2, 0],
+        [0, 2, 2]
+    ],
+
+    [
+        [0, 3, 0],
+        [0, 3, 0],
+        [0, 3, 3]
+    ],
+
 ]
 
+class Player {
+    constructor(matrix, x, y, color) {
+        this.matrix = matrix
+        this.x = x
+        this.y = y
+        this.color = color
+    }
+    draw() {
+        // console.log(this)
+        ctx.fillStyle = this.color
+        this.matrix.forEach((row, j) => {
+
+            row.forEach((cell, i) => {
+                if (cell > 0) {
+                    // console.log(`x:  ${calc(this.x, i)}  y: ${calc(this.y, 'y', j)}`)
+                    ctx.beginPath()
+                    ctx.rect(calc(this.x, i), calc(this.y, 'y', j), pixel, pixel)
+                    ctx.fill()
+                    ctx.closePath()
+                    ctx.fillRect(calc(this.x, i), calc(this.y, 'y', j), pixel, pixel)
+                }
+
+            })
+        });
+    }
+
+    down() {
+        this.y++
+
+
+    }
+    left() {
+        this.x = this.x--
+            // console.log(this.x + ' new x')
+    }
+
+    right() {
+        this.x = this.x++
+            // console.log(this.x + ' new x')
+    }
+
+}
 const xPixels = canvas.width / pixel
 const yPixels = canvas.height / pixel
 
@@ -19,11 +114,11 @@ function drawLine(startX, startY, toX, toY, ) {
     ctx.beginPath()
     ctx.moveTo(startX, startY)
     ctx.lineTo(toX, toY)
-    ctx.strokeStyle = 'rgba(0,0,0,0.4)'
+    ctx.strokeStyle = 'rgba(248, 218, 51, 0.3)'
     ctx.stroke()
 
     ctx.closePath()
-    console.log('done')
+
 }
 
 function setUpStage() {
@@ -44,4 +139,43 @@ function setUpStage() {
     }
 
 }
-setUpStage()
+
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    setUpStage()
+    if (currPlayer.inPlay) {
+        if (!currPlayer.collided && !currPlayer.canStart) {
+            currPlayer.shape.down()
+            currPlayer.shape.draw()
+            console.log(currPlayer)
+        }
+        if (!currPlayer.collided && currPlayer.canStart) {
+            currPlayer.shape = new Player(player[2], 5, -1, 'red')
+            currPlayer.shape.draw()
+            currPlayer.canStart = false
+        }
+
+
+
+
+
+
+
+        currPlayer.time = frame
+    }
+
+
+    window.setInterval(function() {
+
+        requestAnimationFrame(animate)
+    }, 3000)
+
+
+
+
+}
+
+
+
+animate()
